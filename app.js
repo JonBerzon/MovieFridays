f2fb99ddfd62da80701f90769023d5c1000955e4
yudagn <yudagn@gmail.com>
2021-11-14 21:15:10 -0500

    heroku deploy take 2

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const users = require("./routes/api/users");
-const bodyParser = require('body-parser');
-const User = require('./models/User')
-const passport = require('passport')
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+const passport = require("passport");
 require("dotenv").config();
-const db = process.env.REACT_APP_MONGO_URI
+const db = process.env.REACT_APP_MONGO_URI;
+const path = require("path");
 
+app.use(express.static("frontend/build"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 app.use("/api/users", users);
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(process.env));
