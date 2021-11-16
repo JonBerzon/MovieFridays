5f9a0f65442142700965b914190061be62139d51
Maisie Bruno-Tyne <maisiejillb@gmail.com>
2021-11-15 18:26:46 -0800

    adding signup css

--- frontend/src/components/session/signup_form.jsx
+++ frontend/src/components/session/signup_form.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import bunny from '../../avatars/avatar-bunny.svg'
+import cat from '../../avatars/avatar-cat.svg'
+import dog from '../../avatars/avatar-dog.svg'
+import dragon from '../../avatars/avatar-dragon.svg'
+import lion from '../../avatars/avatar-lion.svg'
+import monkey from '../../avatars/avatar-monkey.svg'
+import panda from '../../avatars/avatar-panda.svg'
+import unicorn from '../../avatars/avatar-unicorn.svg'
+
+
+
 import { withRouter } from 'react-router-dom';
-const AVATAR_PREFIX = 'http://127.0.0.1:5500/'
+
+
 
 class SignupForm extends React.Component {
     constructor(props) {
@@ -63,72 +75,78 @@ class SignupForm extends React.Component {
 
     renderAvatars() {
       return(
-        <div className="signup-avatar-container">
-          <input  onClick={this.update('avatar')} type="checkbox" id="avatar1" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-bunny.svg`}/>
-          <label className="signup-avatar" htmlFor="avatar1" onClick={() => this.toggleSelected('cb1')}>
-            <div name="check" className="checkbox-background" id="cb1">
-              <img className='avatar-image'  src={AVATAR_PREFIX + `frontend/src/avatars/avatar-bunny.svg`} alt="bunny" />
-            </div>
-          </label>
+        <div className="signup-avatar-container" >
+          <h1>Select an Avatar</h1>
 
-          <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar2" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-cat.svg`} />
-          <label className="signup-avatar" htmlFor="avatar2" onClick={() => this.toggleSelected('cb2')}>
-            <div name="check" className="checkbox-background" id="cb2">
-              <img className='avatar-image' src={AVATAR_PREFIX + `frontend/src/avatars/avatar-cat.svg`} alt="cat" />
-            </div>
-          </label>
+          <div>
+            <input  onClick={this.update('avatar')} type="checkbox" id="avatar1" value={bunny}/>
+            <label className="signup-avatar" htmlFor="avatar1" onClick={() => this.toggleSelected('cb1')}>
+              <div name="check" className="checkbox-background" id="cb1">
+                <img className='avatar-image'  src={bunny} alt="bunny" />
+              </div>
+            </label>
 
-          <input  className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar3" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-dog.svg`}/>
-          <label className="signup-avatar" htmlFor="avatar3" onClick={() => this.toggleSelected('cb3')}>
-            <div name="check" className="checkbox-background" id="cb3">
-              <img className='avatar-image' src={AVATAR_PREFIX + `frontend/src/avatars/avatar-dog.svg`} alt="dog" />
-            </div>
-          </label>
+            <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar2" value={cat} />
+            <label className="signup-avatar" htmlFor="avatar2" onClick={() => this.toggleSelected('cb2')}>
+              <div name="check" className="checkbox-background" id="cb2">
+                <img className='avatar-image' src={cat} alt="cat" />
+              </div>
+            </label>
 
-          <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar4" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-dragon.svg`}/>
-          <label className="signup-avatar" htmlFor="avatar4" onClick={() => this.toggleSelected('cb4')}>
-            <div name="check" className="checkbox-background" id="cb4">
-              <img className='avatar-image' src={AVATAR_PREFIX + `frontend/src/avatars/avatar-dragon.svg`} alt="dragon" />
-            </div>
-          </label>
+            <input  className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar3" value={dog}/>
+            <label className="signup-avatar" htmlFor="avatar3" onClick={() => this.toggleSelected('cb3')}>
+              <div name="check" className="checkbox-background" id="cb3">
+                <img className='avatar-image' src={dog} alt="dog" />
+              </div>
+            </label>
 
-          <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar5" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-lion.svg`}/>
-          <label className="signup-avatar" htmlFor="avatar5" onClick={() => this.toggleSelected('cb5')}>
-            <div name="check" className="checkbox-background" id="cb5">
-              <img className='avatar-image' src={AVATAR_PREFIX + `frontend/src/avatars/avatar-lion.svg`} alt="lion" />
-            </div>
-          </label>
+            <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar4" value={dragon}/>
+            <label className="signup-avatar" htmlFor="avatar4" onClick={() => this.toggleSelected('cb4')}>
+              <div name="check" className="checkbox-background" id="cb4">
+                <img className='avatar-image' src={dragon} alt="dragon" />
+              </div>
+            </label>
 
-          <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar6" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-monkey.svg`}/>
-          <label className="signup-avatar" htmlFor="avatar6" onClick={() => this.toggleSelected('cb6')}>
-            <div name="check" className="checkbox-background" id="cb6">
-              <img className='avatar-image' src={AVATAR_PREFIX + `frontend/src/avatars/avatar-monkey.svg`} alt="monkey" />
-            </div>
-          </label>
+            <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar5" value={lion}/>
+            <label className="signup-avatar" htmlFor="avatar5" onClick={() => this.toggleSelected('cb5')}>
+              <div name="check" className="checkbox-background" id="cb5">
+                <img className='avatar-image' src={lion} alt="lion" />
+              </div>
+            </label>
 
-          <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar7" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-panda.svg`}/>
-          <label className="signup-avatar" htmlFor="avatar7" onClick={() => this.toggleSelected('cb7')}>
-            <div name="check" className="checkbox-background" id="cb7">
-              <img className='avatar-image' src={AVATAR_PREFIX + `frontend/src/avatars/avatar-panda.svg`} alt="panda"/>
-            </div>
-          </label>
+            <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar6" value={monkey}/>
+            <label className="signup-avatar" htmlFor="avatar6" onClick={() => this.toggleSelected('cb6')}>
+              <div name="check" className="checkbox-background" id="cb6">
+                <img className='avatar-image' src={monkey} alt="monkey" />
+              </div>
+            </label>
 
-          <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar8" value={AVATAR_PREFIX + `frontend/src/avatars/avatar-unicorn.svg`}/>
-          <label className="signup-avatar" htmlFor="avatar8" onClick={() => this.toggleSelected('cb8')}>
-            <div name="check" className="checkbox-background" id="cb8">
-              <img className='avatar-image' src={AVATAR_PREFIX + `frontend/src/avatars/avatar-unicorn.svg`} alt="unicorn" />
-            </div>
-          </label>
+            <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar7" value={panda}/>
+            <label className="signup-avatar" htmlFor="avatar7" onClick={() => this.toggleSelected('cb7')}>
+              <div name="check" className="checkbox-background" id="cb7">
+                <img className='avatar-image' src={panda} alt="panda"/>
+              </div>
+            </label>
+
+            <input className="checkbox-hidden" onClick={this.update('avatar')} type="checkbox" id="avatar8" value={unicorn}/>
+            <label className="signup-avatar" htmlFor="avatar8" onClick={() => this.toggleSelected('cb8')}>
+              <div name="check" className="checkbox-background" id="cb8">
+                <img className='avatar-image' src={unicorn} alt="unicorn" />
+              </div>
+            </label>
+          </div>
         </div>
       )
     }
 
     render() {
+      console.log(this.state)
       return (
         <div className="signup-background">
 
           <div className="signup-intro">
             <h1>Movie Fridays</h1>
+                  <div id="goldline"></div>
             <p>Blurb blurb blurb</p>
           </div>
 
@@ -141,7 +159,7 @@ class SignupForm extends React.Component {
               </div>
               
               <div className="signup-input">
-                  <div className="signup-input">
+                  <div className="signup-username">
                     <label >Username
                       <input  
                           className="border-bottom-light"
@@ -153,7 +171,7 @@ class SignupForm extends React.Component {
                     </label>
                   </div>
 
-                  <div className="signup-input">
+                  <div className="signup-password">
                     <label>Password
                       <input 
                           className="border-bottom-light"
@@ -167,12 +185,12 @@ class SignupForm extends React.Component {
                   
                   {this.renderErrors()}
                   
-                  <h1 id="select-avatar">Select an Avatar</h1> 
                   {this.renderAvatars()}
                   
-                <input className="signup-buttons .signup" type="submit" value="Submit" />
-                <a className="signup-buttons" href="/login">Demo</a>
-
+                  <div className="signup-buttons">
+                    <input type="submit" value="Submit" />
+                    <a href="/login">Demo</a>
+                  </div>
               </div>
             </form>  
           </div>
