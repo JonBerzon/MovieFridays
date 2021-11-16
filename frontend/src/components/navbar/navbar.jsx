6ed1777f83ca061c4ff2d87886a449ef964013d0
yudagn <yudagn@gmail.com>
2021-11-15 21:40:42 -0500

    comlete navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import catSRC from "../../avatars/avatar-cat.svg";
-import dogSRC from "../../avatars/avatar-dog.svg";
+import { Link } from "react-router-dom";
 
-class Navbar extends React.Component {
-  render() {
-    debugger
-    return (
-      <div className="navbar-flex-container">
-        <div className="navbar-container">
-          <div className="navbar-logo-container">
-            <h3 className="navbar-logo">MOVIE FRIDAYS</h3>
-          </div>
+const Navbar = props => (
+  <div className="navbar-flex-container">
+    <div className="navbar-container">
+      <div className="navbar-logo-container">
+        <Link to='/groups'>
+          <h3 className="navbar-logo">MOVIE FRIDAYS</h3>
+        </Link>
+      </div>
 
-          <div className="navbar-dropdown-button">
-            <div className="navbar-dropdown-img-container">
-              <img className="navbar-user-avatar" src={dogSRC} />
-            </div>
-            <div className="navbar-dropdown-menu">
-              <li className='navbar-logout' onClick={this.props.logout}>Logout</li>
-            </div>
-          </div>
+      <div className="navbar-dropdown-button">
+        <div className="navbar-dropdown-img-container">
+          <img className="navbar-user-avatar" src={props.user.avatar} />
+        </div>
+        <div className="navbar-dropdown-menu">
+          <li className="navbar-logout" onClick={props.logout}>
+            Logout
+          </li>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default Navbar;
