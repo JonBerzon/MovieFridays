6101d9b91e56ad4a11b3b77a3a6cca5613f3fe77
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-11-16 14:46:21 -0500

    movie show page complete outside of group ratings

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -8,11 +8,13 @@ import GroupsIndex from './groups/groups_index';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import NavbarContainer from './navbar/navbar_container';
+import MovieShow from './movie_show/movie_show';
 
 const App = () => (
     <div>
         <ProtectedRoute path='/' component={NavbarContainer} />
         <Switch>
+            <ProtectedRoute exact path="/movie" component={MovieShow} />
             <ProtectedRoute exact path="/groups" component={GroupsIndex} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
