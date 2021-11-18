3992378b7a3e7286959a657d76a0e5a8386fc3fd
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-11-18 11:49:22 -0500

    group rating added to show page

--- frontend/src/components/movie_show/group_ratings.jsx
+++ frontend/src/components/movie_show/group_ratings.jsx
@@ -7,6 +7,15 @@ class GroupRatings extends React.Component{
         let {reviews, group, movie} = this.props
         let metacritic = movie.meta ? movie.meta : "N/A"
         let imdb = movie.imdb ? movie.imdb : "N/A"
+        let groupRating;
+        if (movie.num_reviews) {
+            groupRating =
+                movie.cumulative_reviews / movie.num_reviews;
+            groupRating =
+                groupRating % 1 !== 0 ? groupRating.toFixed(1) : groupRating;
+        } else {
+            groupRating = "?";
+        }
 
         return(
             <div className="group-ratings-div">
@@ -14,6 +23,7 @@ class GroupRatings extends React.Component{
                     <h1>{group.name}</h1>
                     <h2>IMDb: <span>{imdb} / 10</span></h2>
                     <h2>Metacritic: <span>{metacritic} / 100</span></h2>
+                    <h2>Group Rating: <span>{groupRating}</span></h2>
                 </div>
                 <hr />
                 <div className="group-ratings-flex">
